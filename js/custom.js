6d8bbcc65dbd2c5ebe6a003063c6c36092f8b9be
fashifa09-cpu <fashifa09@gmail.com>
2025-10-28 21:40:57 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/smk5.jpeg", 
+      "images/slide/background.jpeg",
+      "images/slide/home2.jpeg"
+    ],  {duration: 2000, fade: 750});
+    
+  })(window.jQuery);
+
+
